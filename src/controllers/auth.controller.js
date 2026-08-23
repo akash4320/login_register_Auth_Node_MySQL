9dd686700586398a4a39931817432aa1e859f657
akash4320 <akash4320@gmail.com>
2026-08-24 00:04:35 +0530

    fix: set secure cookie option to false in login controller

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -21,7 +21,7 @@ export async function login(req, res, next) {
 
     res.cookie("accessToken", token, {
       httpOnly: true,
-      secure: process.env.COOKIE_SECURE === "true",
+      secure: false,
       sameSite: "none",
       path: "/",
     });
